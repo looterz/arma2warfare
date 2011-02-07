b88bee1ef571e6b6a6bdffd5d6ab5ea394bcc764
Evgeny_Zyuzin <Evgeny_Zyuzin@fcf91c93-9c15-691f-260e-a8d136b6ebf9>
2011-02-07 14:15:47 +0000

    remove extra comments

--- arma2.script.compiler/unittest/Program.cs
+++ arma2.script.compiler/unittest/Program.cs
@@ -18,15 +18,6 @@ namespace Obfuscate
             foreach (var fileName in files)
             {
                 string content = File.ReadAllText(fileName);
-
-    //if ($fileLine -match "PROFILER_BEGIN" -or 
-    //    $fileLine -match "PROFILER_END" -or
-    //    $fileLine -match "!isNil ""LogInited""" -or
-		
-    //    $fileLine -match "logging.sqf" -or
-    //    $fileLine -match "profiler.sqf" -or
-    //    $fileLine -match "profiler.h" -or
-    //    $fileLine -match "!isNil ""initProfiler"""
                 GlobalVars.ExcludeLines.Add("PROFILER_BEGIN");
                 GlobalVars.ExcludeLines.Add("PROFILER_END");
                 GlobalVars.ExcludeLines.Add(@"profiler.sqf");
@@ -36,11 +27,6 @@ namespace Obfuscate
                 GlobalVars.ExcludeLines.Add(@"call Log(High|Inform|Medium|Warning|Trace)");
                 GlobalVars.ExcludeLines.Add(@"""(?:[^""\\]|\\.|\""\"")*""\s*call Log(High|Inform|Medium|Warning|Trace)");
                 GlobalVars.ExcludeLines.Add(@"'(?:[^'\\]|\\.|'')*'\s*call Log(High|Inform|Medium|Warning|Trace)");
-
-                //"(?:[^"]|\\.|\"\")+?" call LogMedium
-                //""(?:[^""]|\\.|\""\"")+?" call Log(High|Inform|Medium|Warning|Trace)
-
-
                 Compiler obfuscate = new Compiler();
                 obfuscate.FileName = fileName;
                 obfuscate.HideVars = false;
